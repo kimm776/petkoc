1c288a9d22aa5612f9fe223e9d7dc2a7fe1d62fa
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-09-01 00:00:00 +0900

    Add files via upload

---
+++ petkoc/src/main/java/com/dog/petkoc/PetkocApplication.java
@@ -0,0 +1,13 @@
+package com.dog.petkoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetkocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetkocApplication.class, args);
+	}
+
+}
---
+++ petkoc/src/test/java/com/dog/petkoc/PetkocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog.petkoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetkocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
