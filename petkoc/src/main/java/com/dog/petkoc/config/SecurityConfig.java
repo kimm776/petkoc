47c96a7b514a7ca65fd08e6a4c260cccb3b0626a
lmj <mjlee95613@gmail.com>
2024-09-04 23:37:23 +0900

    application.properties => application.yml로 변경, index.html 추가

--- petkoc/src/main/java/com/dog/petkoc/config/SecurityConfig.java
+++ petkoc/src/main/java/com/dog/petkoc/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/member/register").permitAll()
+                .requestMatchers("/**","/member/register").permitAll()
                 .anyRequest().authenticated()
         );
         http.csrf(csrf -> csrf.disable());
