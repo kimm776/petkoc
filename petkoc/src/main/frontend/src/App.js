627bf32c7b3c056cb6baadb139f76c2220028371
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-10-08 11:24:15 +0900

    react run 문제 처리 & @babel 패키지 설치

--- petkoc/src/main/frontend/src/App.js
+++ petkoc/src/main/frontend/src/App.js
@@ -5,14 +5,18 @@ function App() {
     const [data, setData] = useState('')
 
     useEffect(() => {
-        axios.get('/api/hello')
-            .then(res => setData(res.data))
+        axios.get('/boards')
+            .then(res => {
+                console.log("히히히", res.data); // 콘솔에 데이터 출력
+                setData(res.data)
+            }
+            )
             .catch(err => console.log(err))
     }, []);
 
     return (
         <div>
-            받아온 값 : {data}
+            받아fdsssdd온 값 : {JSON.stringify(data)} {/* 객체를 문자열로 변환해서 출력 */}
         </div>
     );
 }
